d54e874a0a3158db69214e33dd5ce6e533759868
Ahsanul Hasan Kabbo <kabboahsanulhasan@gmail.com>
2022-10-14 23:29:37 +0600

    first commit

---
+++ src/Components/Information.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from 'react';
+import { Area, Bar, CartesianGrid, ComposedChart, Legend, Tooltip, XAxis, YAxis } from 'recharts';
+
+const Information = () => {
+    const [students, setStudents] = useState
+    ([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/students')
+            .then((res) => res.json())
+            .then((data) => setStudents(data));
+    }, []);
+    return (
+        <div>
+            <div className='my-20 w-3/4 mx-auto'>
+                <ComposedChart width={730} height={250} data={students}>
+                    <XAxis dataKey='Name' />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <CartesianGrid stroke="#f5f5f5" />
+                    <Area type="monotone" dataKey="CGPA" fill="#8884d8" stroke="#8884d8" />
+                    <Bar dataKey="Marks" barSize={20} fill="#413ea0" />
+                </ComposedChart>
+            </div>
+            <div class="overflow-x-auto relative m-10">
+                <h1 className='text-2xl text-center font-semibold m-5 text-[#0182BE]'>Student Information</h1>
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs uppercase text-white bg-cyan-500 border-b border-cyan-100">
+                        <tr>
+                            <th scope="col" class="py-3 px-6">
+                                Student Name
+                            </th>
+                            <th scope="col" class="py-3 px-6">
+                                Email
+                            </th>
+                            <th scope="col" class="py-3 px-6">
+                                Student ID
+                            </th>
+                            <th scope="col" class="py-3 px-6">
+                                CGPA
+                            </th>
+                            <th scope="col" class="py-3 px-6">
+                                Department
+                            </th>
+                            <th scope="col" class="py-3 px-6">
+                                Marks
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {students.map((student) => (
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <td className="py-4 px-6">
+                                    {student.Name}
+                                </td>
+                                <td className="py-4 px-6">
+                                    {student.email}
+                                </td>
+                                <td className="py-4 px-6">
+                                    {student.id}
+                                </td>
+                                <td className="py-4 px-6">
+                                    {student.CGPA}
+                                </td>
+                                <td className="py-4 px-6">
+                                    {student.Department}
+                                </td>
+                                <td className="py-4 px-6">
+                                    {student.Marks}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default Information;
---
+++ src/Components/SubmitForm.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const SubmitForm = () => {
+    const { register, handleSubmit, reset } = useForm();
+
+    const onSubmit = (data) => {
+        const newData = { ...data};
+        fetch("http://localhost:5000/informations", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(newData),
+        })
+            .then((res) => res.json())
+            .then((inserted) => {
+                if (inserted.insertedId) {
+                    reset();
+                    toast.success("Add successfully");
+                }
+            });
+    };
+    return (
+        <div>
+            <h1 className="text-2xl text-center font-semibold mt-5 text-[#0182BE]">
+                Student Form{" "}
+            </h1>
+            <div className="ml-3 ">
+                <form
+                    className="lg:w-10/12 mx-auto bg-white shadow-gray-300 border shadow-md rounded py-12 px-5 mt-10 md:w-9/12 sm:w-11/12 sm:mx-auto"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    <div className='lg:flex justify-evenly'>
+                        <div>
+                            <div className="flex items-center">
+                                <label className="label w-48">
+                                    <span className="label-text text-xl">
+                                        Name :
+                                    </span>
+                                </label>
+                                <input
+                                    required
+                                    type="text"
+                                    placeholder="Your Name"
+                                    className="py-2 pl-3 w-6/12 my-1 border border-gray-300 bg-slate-50 rounded outline-none "
+                                    {...register("Name")}
+                                />
+                            </div>
+                            <div className="flex items-center">
+                                <label className="label w-48">
+                                    <span className="label-text text-xl">
+                                        Email :
+                                    </span>
+                                </label>
+                                <input
+                                    required
+                                    type="email"
+                                    placeholder="Your Email"
+                                    className="py-2 pl-3 w-6/12 my-1 border border-gray-300 bg-slate-50 rounded outline-none "
+                                    {...register("email")}
+                                />
+                            </div>
+                            <div className="flex items-center">
+                                <label className="label w-48">
+                                    <span className="label-text text-xl">
+                                        Student ID:
+                                    </span>
+                                </label>
+                                <input
+                                    required
+                                    type="text"
+                                    placeholder="Student ID"
+                                    className="py-2 pl-3 w-6/12 my-1 border border-gray-300 bg-slate-50 rounded outline-none "
+                                    {...register("id")}
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex items-center">
+                                <label className="label w-48">
+                                    <span className="label-text text-xl">
+                                        Depertment:
+                                    </span>
+                                </label>
+                                <input
+                                    required
+                                    type="text"
+                                    placeholder="Department"
+                                    className="py-2 pl-3 w-6/12 my-1 border border-gray-300 bg-slate-50 rounded outline-none "
+                                    {...register("Department")}
+                                />
+                            </div>
+                            <div className="flex items-center">
+                                <label className="label w-48">
+                                    <span className="label-text text-xl">
+                                        CGPA:
+                                    </span>
+                                </label>
+                                <input
+                                    required
+                                    type="text"
+                                    placeholder="CGPA"
+                                    className="py-2 pl-3 w-6/12 my-1 border border-gray-300 bg-slate-50 rounded outline-none "
+                                    {...register("CGPA")}
+                                />
+                            </div>
+
+                            <div>
+                                <div className="flex items-center">
+                                    <label className="label w-48">
+                                        <span className="label-text text-xl">
+                                            Marks:
+                                        </span>
+                                    </label>
+                                    <input
+                                        required
+                                        type="number"
+                                        placeholder="Marks"
+                                        className="py-2 pl-3 w-6/12 my-1 border border-gray-300 bg-slate-50 rounded outline-none "
+                                        {...register("Marks")}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex items-center rounded-lg w-fit mx-auto mt-3">
+                        <button className="flex items-center gap-2 bg-blue-600 py-2 px-6 text-white font-bold rounded  hover:bg-white hover:text-blue-600 hover:outline-1 hover:border hover:border-blue-600 hover: shadow-blue-300 hover: shadow-sm">
+                            {/* <AiFillSave /> */}
+                            <input type="submit" value="Submit" />
+                        </button>
+                    </div>
+                </form>
+            </div>
+            <ToastContainer />
+        </div>
+    );
+};
+
+export default SubmitForm;
